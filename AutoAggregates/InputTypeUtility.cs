717bc7579e50d0c1ae5a453ff24328e8d7d47523
Arman <OneDeveloper@outlook.com>
2024-11-09 11:59:00 +0330

    Add only english user input

---
+++ AutoAggregates/InputTypeUtility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AutoAggregates;
+internal static class InputTypeUtility
+{
+
+	public static string EnglishOnly()
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+		Console.WriteLine("Enter Aggregate Name (English only and press Enter):");
+		StringBuilder input = new StringBuilder();
+
+		while (true)
+		{
+			ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+			// Check if the key is a valid English character or common symbol
+			if (IsEnglish(keyInfo.KeyChar))
+			{
+				// Append the character to input and write to console
+				input.Append(keyInfo.KeyChar);
+				Console.Write(keyInfo.KeyChar);
+			}
+			else if (keyInfo.Key == ConsoleKey.Backspace && input.Length > 0)
+			{
+				// Handle backspace
+				input.Remove(input.Length - 1, 1);
+				Console.Write("\b \b");
+			}
+			else if (keyInfo.Key == ConsoleKey.Enter)
+			{
+				// Handle enter key to end input
+				return FixAggregateName.SafeName(input.ToString());
+			}
+		}
+	}
+
+	private static bool IsEnglish(char c)
+	{
+		// Check if the character is an English letter or common English symbol
+		return c >= ' ' && c <= '~';
+	}
+}
+
+
--- AutoAggregates/Program.cs
+++ AutoAggregates/Program.cs
@@ -1,16 +1,12 @@
 ﻿
 using AutoAggregates;
 
-using System.Reflection;
 
-string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 string workingDirectory = Directory.GetCurrentDirectory();
-Console.WriteLine("Enter Aggregate Name (and press Enter):");
-//string startDirectory = exePath; // مسیر شروع را تنظیم کنید
 string startDirectory = workingDirectory;
-var userInput = FixAggregateName.SafeName(Console.ReadLine());
+var userInput = InputTypeUtility.EnglishOnly();
 string targetFile = userInput; // نام فایل مورد نظر را وارد کنید
-							   //Search.FileInDirectories(startDirectory, targetFile);
+Search.FileInDirectories(startDirectory, targetFile);
 Search.TraverseDirectories(startDirectory, targetFile);
 Console.WriteLine("Press any key to exit. :)");
 Console.ReadKey();
